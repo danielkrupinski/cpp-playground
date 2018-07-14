58fed72624517046bc4cb5cf791510d67bd9878d
Daniel Krupiński <danielkrupinski@outlook.com>
2018-07-14 15:12:24 +0200

    Add tuple example

--- tuple/tuple.cpp
+++ tuple/tuple.cpp
@@ -1,1 +1,9 @@
-//
+#include <iostream>
+#include <tuple>
+
+int main()
+{
+    std::tuple<int, char, bool> tuple{ 123, 'A', true };
+    std::cout << std::get<0>(tuple) << '\n' << std::get<1>(tuple) << '\n' << std::get<2>(tuple);
+    return 0;
+}
