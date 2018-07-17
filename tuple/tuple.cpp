b40c0d96b2dca0c17fc663b536dd385313acf97b
Daniel Krupiński <danielkrupinski@outlook.com>
2018-07-17 16:35:36 +0200

    Add '\n' at the end of output

--- tuple/tuple.cpp
+++ tuple/tuple.cpp
@@ -4,6 +4,6 @@
 int main()
 {
     std::tuple<int, char, bool> tuple{ 123, 'A', true };
-    std::cout << std::get<0>(tuple) << '\n' << std::get<1>(tuple) << '\n' << std::get<2>(tuple);
+    std::cout << std::get<0>(tuple) << '\n' << std::get<1>(tuple) << '\n' << std::get<2>(tuple) << '\n';
     return 0;
 }
