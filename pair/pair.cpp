10b725673ff4299d3d469851c0147bca37d68c37
Daniel Krupiński <danielkrupinski@outlook.com>
2018-07-17 16:35:03 +0200

    Add '\n' at the end of output

--- pair/pair.cpp
+++ pair/pair.cpp
@@ -4,6 +4,6 @@
 int main()
 {
     std::pair<char, int> pair{ 'A', 123 };
-    std::cout << pair.first << '\n' << pair.second << '\n' <<  std::get<0>(pair) << '\n' << std::get<1>(pair);
+    std::cout << pair.first << '\n' << pair.second << '\n' <<  std::get<0>(pair) << '\n' << std::get<1>(pair) << '\n';
     return 0;
 }
