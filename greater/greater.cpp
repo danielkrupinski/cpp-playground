d8893e82a9a81c1449f64a7b9965fd87d86f316b
Daniel Krupiński <danielkrupinski@outlook.com>
2018-07-17 17:15:10 +0200

    Add std::greater example

--- greater/greater.cpp
+++ greater/greater.cpp
@@ -1,1 +1,9 @@
-//
+#include <functional>
+#include <iostream>
+
+int main()
+{
+    std::function<bool(int, int)> less{ std::greater<int>{ } };
+    std::cout << less(1, 10) << '\n' << less(20, 2) << '\n';
+    return 0;
+}
