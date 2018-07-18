6a74e6de3a17925e618119b988a65c4686c1707d
Daniel Krupiński <danielkrupinski@outlook.com>
2018-07-18 17:17:32 +0200

    Add std::tie example

--- tie/tie.cpp
+++ tie/tie.cpp
@@ -1,1 +1,12 @@
-//
+#include <iostream>
+#include <tuple>
+
+int main()
+{
+    std::tuple<int, char, bool> tuple{ 10, 'A', true };
+    int a;
+    bool b;
+    std::tie(a, std::ignore, b) = tuple;
+    std::cout << a << '\n' << b << '\n';
+    return 0;
+}
