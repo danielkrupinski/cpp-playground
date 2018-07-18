40151d879da2087d6f0a875895c76b5b5bbbedcf
Daniel Krupiński <danielkrupinski@outlook.com>
2018-07-18 15:46:09 +0200

    Add std::vector.emplace_back() example

--- emplace_back/emplace_back.cpp
+++ emplace_back/emplace_back.cpp
@@ -1,1 +1,17 @@
-//
+#include <iostream>
+#include <vector>
+
+struct Foo {
+    int a;
+    std::string s;
+    
+    Foo(int _a, const std::string& _s) : a{ _a }, s{ _s } { }
+};
+
+int main()
+{
+    std::vector<Foo> foo;
+    foo.emplace_back(2018, "C++");
+    std::cout << foo[0].a << '\n' << foo[0].s << '\n';
+    return 0;
+}
