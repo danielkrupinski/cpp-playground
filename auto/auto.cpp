edd2c788d9dc93d83d900cfe7a2234d1154bda48
Daniel Krupiński <danielkrupinski@outlook.com>
2018-07-22 13:38:31 +0200

    Add auto example

--- auto/auto.cpp
+++ auto/auto.cpp
@@ -1,1 +1,16 @@
-//
+#include <iostream>
+
+constexpr decltype(auto) square(int a) noexcept
+{
+    return a * a;
+}
+
+int main()
+{
+    auto i = 12;
+    auto f = 1.123f;
+    auto d = 3.21;
+    auto b = true;
+    std::cout << square(i) << std::endl;
+    return 0;
+}
