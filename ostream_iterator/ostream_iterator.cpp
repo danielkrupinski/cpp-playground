cc31698a053f5875d93e8b3ab8545374e78c6894
Daniel Krupiński <danielkrupinski@outlook.com>
2018-07-24 18:02:17 +0200

    Add ostream_iterator example

--- ostream_iterator/ostream_iterator.cpp
+++ ostream_iterator/ostream_iterator.cpp
@@ -1,1 +1,10 @@
-//
+#include <iostream>
+#include <iterator>
+#include <vector>
+
+int main()
+{
+    std::vector<int> v{ 1, 2, 3, 123, 234, 345 };
+    std::copy(v.begin(), v.end(), std::ostream_iterator<int>{ std::cout, "\n" });
+    return 0;
+}
