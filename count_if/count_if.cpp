89151870f192747d1b4cf5fa91a0c7675438bd0e
Daniel Krupiński <danielkrupinski@outlook.com>
2018-08-14 15:45:02 +0200

    Add count_if example

--- count_if/count_if.cpp
+++ count_if/count_if.cpp
@@ -1,1 +1,10 @@
-//
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    const std::vector<int> v{ 1, 2, 1, 138, 1, 0, -1813, -173, -88, 1, 15, 47, 1 };
+    std::cout << "odd numbers count: " << std::count_if(v.begin(), v.end(), [](int a) { return a & 1; });
+    return 0;
+}
