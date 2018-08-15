0823c81289600d93a6dfa6b179aab1c4955f9755
Daniel Krupiński <danielkrupinski@outlook.com>
2018-08-15 16:43:06 +0200

    Update std::for_each_n example

--- for_each_n/for_each_n.cpp
+++ for_each_n/for_each_n.cpp
@@ -4,5 +4,7 @@
 
 int main()
 {
+    std::vector<int> v{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    std::for_each_n(v.begin(), 7, [](int a) { a & 1 ? std::cout << a << " is odd\n" : std::cout << a << " is even\n"; });
     return 0;
 }
