a76b7e4307dca2dc65d97fb578aa8b017987ba83
Daniel Krupiński <danielkrupinski@outlook.com>
2018-08-15 11:09:40 +0200

    Add \n at the end of output

--- defaultfloat/defaultfloat.cpp
+++ defaultfloat/defaultfloat.cpp
@@ -3,6 +3,6 @@
 int main()
 {
     float f = 1.234f;
-    std::cout << "f = " << std::hexfloat << f << "\nf = " << std::defaultfloat << f;
+    std::cout << "f = " << std::hexfloat << f << "\nf = " << std::defaultfloat << f << '\n';
     return 0;
 }
