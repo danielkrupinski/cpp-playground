9f6def070f5a6e694bdecd710baa85a1e3216a1c
Daniel Krupiński <danielkrupinski@outlook.com>
2018-08-15 14:27:34 +0200

    Update std::copy_if example

--- copy_if/copy_if.cpp
+++ copy_if/copy_if.cpp
@@ -6,5 +6,14 @@
 
 int main()
 {
+    std::vector<int> v{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, };
+    std::vector<int> v1;
+
+    std::copy_if(v.begin(), v.end(), std::back_inserter(v1), [](int a) { return !(a & 1); });
+    std::cout << "v: ";
+    std::copy(v.begin(), v.end(), std::ostream_iterator<int>{ std::cout, " " });
+    std::cout << '\n' << "v1: ";
+    std::copy(v1.begin(), v1.end(), std::ostream_iterator<int>{ std::cout, " " });
+    std::cout << '\n';
     return 0;
 }
