f9b4aeee3287125fc0c56feaed01448497d8b739
Daniel Krupiński <danielkrupinski@outlook.com>
2018-08-15 13:12:14 +0200

    Update std::optional example

--- optional/optional.cpp
+++ optional/optional.cpp
@@ -3,5 +3,8 @@
 
 int main()
 {
+    std::optional<std::string> s{ "std::optional test" };
+    std::optional<std::string> s1{ std::nullopt };
+    std::cout << "s = " << s.value_or("empty") << '\n' << "s1 = " << s1.value_or("empty") << '\n';
     return 0;
 }
