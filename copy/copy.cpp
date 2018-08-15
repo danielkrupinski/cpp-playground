bedca5646185774840f1d8ae9363cde019a8b4be
Daniel Krupiński <danielkrupinski@outlook.com>
2018-08-15 14:13:34 +0200

    Update std::copy example

--- copy/copy.cpp
+++ copy/copy.cpp
@@ -5,5 +5,14 @@
 
 int main()
 {
+    std::vector<int> v{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, };
+    std::vector<int> v1;
+
+    std::copy(v.begin(), v.begin() + 5, std::back_inserter(v1));
+    std::cout << "v: ";
+    std::copy(v.begin(), v.end(), std::ostream_iterator<int>{ std::cout, " " });
+    std::cout << '\n' << "v1: ";
+    std::copy(v1.begin(), v1.end(), std::ostream_iterator<int>{ std::cout, " " });
+    std::cout << '\n';
     return 0;
 }
