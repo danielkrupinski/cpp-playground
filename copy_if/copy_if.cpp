500137a1e7847db09aae1f2b8d9bea497449b1a4
Daniel Krupiński <danielkrupinski@outlook.com>
2018-08-15 15:17:55 +0200

    Update formatting

--- copy_if/copy_if.cpp
+++ copy_if/copy_if.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-    std::vector<int> v{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, };
+    std::vector<int> v{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
     std::vector<int> v1;
 
     std::copy_if(v.begin(), v.end(), std::back_inserter(v1), [](int a) { return !(a & 1); });
