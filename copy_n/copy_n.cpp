54451e10947cfad8d8b4fef8897434771caee9b2
Daniel Krupiński <danielkrupinski@outlook.com>
2018-08-15 15:29:55 +0200

    Update copy_n example

--- copy_n/copy_n.cpp
+++ copy_n/copy_n.cpp
@@ -5,5 +5,8 @@
 
 int main()
 {
+    std::vector<int> v{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    std::copy_n(v.begin(), 5, std::ostream_iterator<int>{ std::cout, " " });
+    std::cout << '\n';
     return 0;
 }
