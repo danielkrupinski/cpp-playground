192b2e24bc5e034223ed45290ab8ab8cbf4f6d34
Daniel Krupiński <danielkrupinski@outlook.com>
2018-08-15 16:08:06 +0200

    Update std::distance example

--- distance/distance.cpp
+++ distance/distance.cpp
@@ -4,5 +4,7 @@
 
 int main()
 {
+    std::vector<int> v{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    std::cout << "distance between v.begin() and v.end(): " << std::distance(v.begin(), v.end()) << '\n';
     return 0;
 }
