a7db820bb6b2243235fe41ba1f6642c861af289f
Daniel Krupiński <danielkrupinski@outlook.com>
2018-08-15 12:57:53 +0200

    Update std::fill example

--- fill/fill.cpp
+++ fill/fill.cpp
@@ -5,5 +5,9 @@
 
 int main()
 {
+    std::vector<int> v{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    std::fill(v.begin(), v.end(), 7);
+
+    std::copy(v.begin(), v.end(), std::ostream_iterator<int>{ std::cout, " " });
     return 0;
 }
