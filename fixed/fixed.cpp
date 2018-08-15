53b433238d6389e780c1eae2f29e836096146da2
Daniel Krupiński <danielkrupinski@outlook.com>
2018-08-15 16:15:23 +0200

    Update std::fixed example

--- fixed/fixed.cpp
+++ fixed/fixed.cpp
@@ -3,6 +3,6 @@
 int main()
 {
     float f = 1.23f;
-    std::cout << "f = " << f << "\nf = " << std::fixed << f << '\n';
+    std::cout << "f = " << f << '\n' << "f = " << std::fixed << f << '\n';
     return 0;
 }
