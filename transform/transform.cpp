8152840d08416867018cc00164a661470f873a32
Daniel Krupiński <danielkrupinski@outlook.com>
2018-08-16 20:42:26 +0200

    Update std::transform

--- transform/transform.cpp
+++ transform/transform.cpp
@@ -6,6 +6,8 @@
 int main()
 {
     std::vector<int> v{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    std::copy(v.begin(), v.end(), std::ostream_iterator<int>{ std::cout, " " });
     std::transform(v.begin(), v.end(), v.begin(), [](int& a) { a += 10; });
+    std::copy(v.begin(), v.end(), std::ostream_iterator<int>{ std::cout, " " });
     return 0;
 }
