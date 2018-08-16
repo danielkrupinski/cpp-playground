297ef23ebf306c2b86739f121c7163ee05b6fbf1
Daniel Krupiński <danielkrupinski@outlook.com>
2018-08-16 20:18:37 +0200

    Update std::reference_wrapper example

--- reference_wrapper/reference_wrapper.cpp
+++ reference_wrapper/reference_wrapper.cpp
@@ -9,6 +9,9 @@ int main()
     int a = 12;
     int b = 17;
     int c = 22;
+    std::cout << "a = " << a << '\n' << "b = " << b << '\n' << "c = " << c << "\n\n";
     std::vector<std::reference_wrapper<int>> v{ a, b, c };
+    std::for_each(v.begin(), v.end(), [](std::reference_wrapper<int> r) { r.get() = 33; });
+    std::cout << "a = " << a << '\n' << "b = " << b << '\n' << "c = " << c << '\n';
     return 0;
 }
