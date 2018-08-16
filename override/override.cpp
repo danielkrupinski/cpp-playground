9fbbbb4418471b2daebe7e6bec633fd71c6f468b
Daniel Krupiński <danielkrupinski@outlook.com>
2018-08-16 19:54:31 +0200

    Add Bar::foo() implementation with override keyword

--- override/override.cpp
+++ override/override.cpp
@@ -4,3 +4,8 @@ class Foo {
 public:
     virtual void foo() { std::cout << "Foo::foo()\n"; }
 };
+
+class Bar final : public Foo {
+public:
+    void foo() override { std::cout << "Bar::foo()\n"; }
+};
