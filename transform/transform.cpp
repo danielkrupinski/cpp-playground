f6a0d2ff28b51c16ee6fbdeb87d06e04c226ae2b
Daniel Krupiński <danielkrupinski@outlook.com>
2018-08-16 20:42:42 +0200

    Update std::transform example

--- transform/transform.cpp
+++ transform/transform.cpp
@@ -7,7 +7,7 @@ int main()
 {
     std::vector<int> v{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
     std::copy(v.begin(), v.end(), std::ostream_iterator<int>{ std::cout, " " });
-    std::transform(v.begin(), v.end(), v.begin(), [](int& a) { a += 10; });
-    std::copy(v.begin(), v.end(), std::ostream_iterator<int>{ std::cout, " " });
+    std::transform(v.begin(), v.end(), v.begin(), [](int a) { return a + 10; });
+    std::copy(v.begin(), v.end(), std::ostream_iterator<int>{ std::cout << '\n', " " });
     return 0;
 }
