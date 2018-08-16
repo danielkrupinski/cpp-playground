b24381a51544ab423bd8ddbb926fbdb59f93b0b0
Daniel Krupiński <danielkrupinski@outlook.com>
2018-08-16 20:05:22 +0200

    Add 3 int variables

--- reference_wrapper/reference_wrapper.cpp
+++ reference_wrapper/reference_wrapper.cpp
@@ -4,6 +4,9 @@
 
 int main()
 {
-    std::vector<std::reference_wrapper<int>> v;
+    int a = 12;
+    int b = 17;
+    int c = 22;
+    std::vector<std::reference_wrapper<int>> v{ a, b, c };
     return 0;
 }
