33bd2bf177e21885ba428785cdb9d69d4ed3a9ec
Daniel Krupiński <danielkrupinski@outlook.com>
2018-08-16 20:50:41 +0200

    Update std::variant example

--- variant/variant.cpp
+++ variant/variant.cpp
@@ -3,5 +3,9 @@
 
 int main()
 {
+    std::variant<int, double> v = 20;
+    std::cout << "v = " << std::get<int>(v) << '\n';
+    v = 33.33;
+    std::cout << "v = " << std::get<double>(v) << '\n';
     return 0;
 }
