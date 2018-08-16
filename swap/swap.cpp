1d080b71f7c1c16fb4c586b48a8b66ff215ba446
Daniel Krupiński <danielkrupinski@outlook.com>
2018-08-16 20:24:36 +0200

    Update std::swap example

--- swap/swap.cpp
+++ swap/swap.cpp
@@ -3,5 +3,10 @@
 
 int main()
 {
+    int a = 2;
+    int b = 50;
+    std::cout << "a = " << a << '\n' << "b = " << b << '\n' << '\n';
+    std::swap(a, b);
+    std::cout << "a = " << a << '\n' << "b = " << b << '\n';
     return 0;
 }
