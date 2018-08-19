9b13ea816990bc5889974a4daebb9249127971a8
Daniel Krupiński <danielkrupinski@outlook.com>
2018-08-19 15:18:37 +0200

    Update std::deque example

--- deque/deque.cpp
+++ deque/deque.cpp
@@ -4,5 +4,10 @@
 
 int main()
 {
+    std::deque<int> d{ 1, 2, 3, 4, 5 };
+    d.pop_back();
+    d.push_front(0);
+    std::copy(d.begin(), d.end(), std::ostream_iterator<int>{ std::cout, " " });
+    std::cout << '\n';
     return 0;
 }
