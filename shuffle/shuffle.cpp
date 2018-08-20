45c2a9fc52d521f33ef2d0451c4c8add411a411b
Daniel Krupiński <danielkrupinski@outlook.com>
2018-08-20 18:02:45 +0200

    Update std::shuffle example

--- shuffle/shuffle.cpp
+++ shuffle/shuffle.cpp
@@ -7,5 +7,9 @@
 
 int main()
 {
+    std::vector<int> v{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    std::shuffle(v.begin(), v.end(),
+                 std::default_random_engine{ std::chrono::system_clock::now().time_since_epoch().count() });
+    std::copy(v.begin(), v.end(), std::ostream_iterator<int>{ std::cout, " " });
     return 0;
 }
